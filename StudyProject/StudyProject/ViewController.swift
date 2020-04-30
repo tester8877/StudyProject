b4367536680c89d9845cf619b19cb87364393b20
Siarhei Huminski <tester8877@yahoo.com>
2020-04-30 14:28:39 -0500

    creatProject

---
+++ StudyProject/StudyProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudyProject
+//
+//  Created by Siarhei Huminski on 4/30/20.
+//  Copyright © 2020 Siarhei Huminski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
